186fb36b8b526001d14b039583fd29919c7c7820
prem <prem.sherpa@codeimmersives.com>
2020-06-22 13:08:41 -0400

    first node app-pay the bill

--- backend.js
+++ backend.js
@@ -1,17 +1,16 @@
 function divideTheBill(totalBill, tipPercentage, noOfPeople, fourthNo){
   if(fourthNo){
-    return "input the numbers in correct format. \ntotal bill\ntip percentage \nno of people \nand that's it\ndon't put the fourth number."
-  } else {
-  
-  const perPerson = (totalBill + (tipPercentage/100) * totalBill) / noOfPeople;
+    return "Please enter the numbers in correct format. \n1. Total bill\n2. Tip percentage \n3. Numbers of people \nAnd that's it, don't put the fourth number."
+  } else if(!totalBill){
+    return "Please enter the numbers in correct format. \n1. Total bill\n2. Tip percentage \n3. Numbers of people \nAnd that's it, don't put the fourth number."
+  }else {
+    const perPerson = (totalBill + (tipPercentage/100) * totalBill) / noOfPeople;
   return perPerson.toFixed(2);
   }
 
 
 }
 
- const z=divideTheBill(100,20,2);
- z;
 
 
 
--- main.js
+++ main.js
@@ -11,19 +11,23 @@ const peopleNoTips = Number(getInput(1));//made extra var peopleNoTips to make i
 const calculate = payTheBill(total, percentage, people, fourthNo);
 const calculateNoTips = payNoTips(total,peopleNoTips);
 
+
+
 if (total && percentage && people && fourthNo){
     console.log( calculate);//function
-}
-else if(total && percentage && people && !fourthNo){
+}else if(total && percentage && people && !fourthNo){
     console.log("Each person pays: $"+calculate+", in total.");
 } else if (total && peopleNoTips){
     console.log("Each person pays: $"+calculateNoTips+ "\nPlease pay your own tips.")
 } else if (total){
     console.log("Could you pay with 20% tips: $"+calculateNoTips+", in total" );
-}else {
-    console.log(`Please enter the total bill, tip percentage \nand total number of people.
-    OR \nPlease enter the total bill, and total numbers of people,\nif you want to pay the tips separately.`)
-}
+}else if (!total){
+    console.log(calculate);
+} 
+
+
+
+
 
 
 
